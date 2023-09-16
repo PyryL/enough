798ce6e1f4b27e35a555abac8c5ed9afccf86d08
Pyry Lahtinen <pyry@pyry.info>
2023-09-16 09:06:33 +0300

    Initial Commit

---
+++ digitlessTimer/digitlessTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  digitlessTimerApp.swift
+//  digitlessTimer
+//
+//  Created by Pyry Lahtinen on 16.9.2023.
+//
+
+import SwiftUI
+
+@main
+struct digitlessTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
