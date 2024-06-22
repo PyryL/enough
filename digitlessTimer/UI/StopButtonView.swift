cf714a11426a8b70e9b0bb5197b177987dd455e2
Pyry Lahtinen <pyry@pyry.info>
2024-06-22 14:40:44 +0300

    long press for red stop button

--- digitlessTimer/UI/StopButtonView.swift
+++ digitlessTimer/UI/StopButtonView.swift
@@ -9,26 +9,22 @@ import SwiftUI
 
 struct StopButtonView: View {
     @ObservedObject var manager: TimerManager
+    @State private var isPressed: Bool = false
     
     var body: some View {
-        Button(action: manager.resetTimer) {
-            Image(systemName: "pause.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 120, height: 120)
-                .foregroundColor(.black.opacity(0.2))
-                .padding()
-        }
-        .buttonStyle(StopButtonStyle())
-        .preventSleep()
-    }
-}
-
-fileprivate struct StopButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .scaleEffect(configuration.isPressed ? 1.1 : 1.0)
-            .animation(.bouncy, value: configuration.isPressed)
+        Image(systemName: "pause.fill")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 120, height: 120)
+            .foregroundColor(.black.opacity(0.2))
+            .padding()
+            .onLongPressGesture(
+                minimumDuration: manager.state == .green ? .leastNormalMagnitude : 1.5,
+                perform: manager.resetTimer,
+                onPressingChanged: { isPressed = $0 })
+            .preventSleep()
+            .scaleEffect(isPressed ? 1.1 : 1.0)
+            .animation(.bouncy, value: isPressed)
     }
 }
 
