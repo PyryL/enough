94de5cd3834be83e53d4a4ef2f383dce6e987fa4
Pyry Lahtinen <pyry@pyry.info>
2024-06-22 16:00:52 +0300

    stop button long press animation

--- digitlessTimer/UI/StopButtonView.swift
+++ digitlessTimer/UI/StopButtonView.swift
@@ -6,11 +6,48 @@
 //
 
 import SwiftUI
+import Combine  // for AnyCancellable
 
 struct StopButtonView: View {
     @ObservedObject var manager: TimerManager
-    @State private var isPressed: Bool = false
-    
+    @State private var pressStart: Date? = nil
+    @State private var pressPercentage: Double = 0
+    @State private var pressTimer: AnyCancellable? = nil
+
+    private func pressChanged(_ isPressed: Bool) {
+        if isPressed {
+            pressStart = .now
+            let secPerFrame = 1.0 / Double(UIScreen.main.maximumFramesPerSecond)
+            pressTimer = Timer
+                .publish(every: secPerFrame, on: .main, in: .default)
+                .autoconnect()
+                .sink { _ in
+                    guard let pressStart else { return }
+                    let duration = -pressStart.timeIntervalSinceNow
+                    pressPercentage = min(1.0, duration / minimumDuration)
+                }
+        } else {
+            pressTimer?.cancel()
+            pressTimer = nil
+            pressStart = nil
+            pressPercentage = 0
+        }
+    }
+
+    private var minimumDuration: Double {
+        manager.state == .green ? .leastNormalMagnitude : 0.7
+    }
+
+    private var scale: CGFloat {
+        guard pressStart != nil else {
+            return 1.0
+        }
+        guard manager.state != .green else {
+            return 0.9
+        }
+        return 0.2 * pressPercentage + 0.8
+    }
+
     var body: some View {
         Image(systemName: "pause.fill")
             .resizable()
@@ -19,12 +56,12 @@ struct StopButtonView: View {
             .foregroundColor(.black.opacity(0.2))
             .padding()
             .onLongPressGesture(
-                minimumDuration: manager.state == .green ? .leastNormalMagnitude : 1.5,
+                minimumDuration: minimumDuration,
                 perform: manager.resetTimer,
-                onPressingChanged: { isPressed = $0 })
+                onPressingChanged: pressChanged)
+            .scaleEffect(scale)
+            .animation(.bouncy, value: pressPercentage)
             .preventSleep()
-            .scaleEffect(isPressed ? 1.1 : 1.0)
-            .animation(.bouncy, value: isPressed)
     }
 }
 
